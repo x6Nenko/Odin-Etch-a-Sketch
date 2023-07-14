ccce4d8a60a8837016f435cb3d3a4a05497bf37d
Max I <clickkava@gmail.com>
2023-07-14 21:08:34 +0300

    Add functions to create random color
    Randomizer creates random value from 0 to 255.
    AddRandomColor sets the current color to randomly generated values in
    the RGB format.
    AddHoverEffect can now use the global variable currentColor to set a
    new color.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const rangeBtn = document.getElementById('range');
 let rangeValue = 0;
 let gridSystem = 16 * 16;
 let isMouseDown = false;
+let currentColor = "";
 
 document.addEventListener("mousedown", function() {
     isMouseDown = true;
@@ -15,15 +16,43 @@ document.addEventListener("mouseup", function() {
     console.log(isMouseDown);
 });
 
+function randomizer() {
+    return Math.floor(Math.random() * 256);
+};
+
+function addRandomColor() {
+    let red = randomizer();
+    let green = randomizer();
+    let blue = randomizer();
+
+    return currentColor = `rgb(${red}, ${green}, ${blue})`;
+};
+
+// function addHoverEffect() {
+//     const allSquares = document.querySelectorAll('.square');
+//     allSquares.forEach(square => {
+//         square.onmousedown = function() {
+//             square.style.backgroundColor = "#000";
+//         };
+//         square.onmouseover = function() {
+//             if (isMouseDown) {
+//                 square.style.backgroundColor = "#000";
+//             }
+//         };
+//     });
+// }
+
 function addHoverEffect() {
     const allSquares = document.querySelectorAll('.square');
     allSquares.forEach(square => {
         square.onmousedown = function() {
-            square.style.backgroundColor = "#000";
+            addRandomColor();
+            square.style.backgroundColor = currentColor;
         };
         square.onmouseover = function() {
             if (isMouseDown) {
-                square.style.backgroundColor = "#000";
+                addRandomColor();
+                square.style.backgroundColor = currentColor;
             }
         };
     });
